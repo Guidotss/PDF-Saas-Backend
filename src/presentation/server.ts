d3b213c4b2deed83368de1baf83951cce58181f6
Guidotss <guidoolguin@hotmail.com>
2024-01-22 21:21:18 -0300

    Terminando configuracion inicial del servidor

---
+++ src/presentation/server.ts
@@ -0,0 +1,35 @@
+import express, { Router, urlencoded } from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+interface ServerOptions {
+  port: number;
+  routes: Router;
+}
+
+export class Server {
+  private readonly app = express();
+  private readonly port: number;
+  private readonly routes: Router;
+
+  constructor(options: ServerOptions) {
+    this.port = options.port;
+    this.routes = options.routes;
+  }
+
+  async start() {
+    this.app.use(morgan("dev"));
+    this.app.use(
+      cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+      })
+    );
+    this.app.use(express.json());
+    this.app.use(urlencoded({ extended: false }));
+    this.app.use(this.routes);
+    this.app.listen(this.port, () => {
+        console.log(`Server running on port ${this.port}`);
+    }); 
+  }
+}
