50d70dad0ae87d7a8f9eee44e905f661d68700e4
Guidotss <guidoolguin@hotmail.com>
2024-02-09 17:35:25 -0300

    Terminando auth entity

--- src/domain/datasources/auth/auth.datasource.ts
+++ src/domain/datasources/auth/auth.datasource.ts
@@ -1,7 +1,8 @@
 import { LoginDto } from "../../dtos/auth/login.dto";
 import { RegisterDto } from "../../dtos/auth/register.dto";
+import { AuthEntity } from "../../entities/auth/auth.entity";
 
 export abstract class AuthDataSource {
-  abstract login(): Promise<LoginDto>;
-  abstract register(): Promise<RegisterDto>; 
+  abstract login(userData: LoginDto): Promise<AuthEntity>;
+  abstract register(userData: RegisterDto): Promise<AuthEntity>;
 }
