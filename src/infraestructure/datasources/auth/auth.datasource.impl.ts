231eb5e937e49c7cf3091aa2c9a0a8125d2b0cf7
Guidotss <guidoolguin@hotmail.com>
2024-02-09 20:53:59 -0300

    creando metodo para hacer login de usuario

--- src/infraestructure/datasources/auth/auth.datasource.impl.ts
+++ src/infraestructure/datasources/auth/auth.datasource.impl.ts
@@ -1,27 +1,51 @@
 import bcrypt from "bcrypt";
+import { prisma } from "../../../data/mongo";
 import { AuthDataSource } from "../../../domain/datasources/auth/auth.datasource";
 import { LoginDto } from "../../../domain/dtos/auth/login.dto";
 import { RegisterDto } from "../../../domain/dtos/auth/register.dto";
+import { AuthEntity } from "../../../domain/entities/auth/auth.entity";
+import { CustomError } from "../../../domain/errors/custom.error";
 
 export class AuthDataSourceImpl implements AuthDataSource {
+  private hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
 
-    private hashPassword(password: string): Promise<string> {
-        return bcrypt.hash(password, 10);
-    }
+  private comparePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
 
-    private comparePassword(password: string, hash: string): Promise<boolean> {
-        return bcrypt.compare(password, hash);
-    }
+  private async getUserByEmail(email: string): Promise<AuthEntity> {
+    try {
+      if (!email) throw new CustomError("Email is required", 400);
+      const user = await prisma.user.findUnique({ where: { email } });
 
-    login(): Promise<LoginDto> {
-        try{
-            
-        }catch(error){
-            console.log(error);
-        }
-    }
-    register(): Promise<RegisterDto> {
-        throw new Error("Method not implemented.");
-    }
+      if (!user) throw new CustomError("User not found", 404);
 
+      return AuthEntity.fromObject(user);
+    } catch (error) {
+      console.log(error);
+      throw new CustomError("Error getting user by email", 500);
+    }
+  }
+
+  async login(userData: LoginDto): Promise<AuthEntity> {
+    try {
+      const user = await this.getUserByEmail(userData.email);
+      if (!user) throw new CustomError("User not found", 404);
+      const isValidPassword = this.comparePassword(
+        userData.password,
+        user.password
+      );
+      if (!isValidPassword) throw new CustomError("Invalid password", 400);
+
+      return AuthEntity.fromObject(user);
+    } catch (error) {
+      console.log(error);
+      throw new CustomError("Error getting user by email", 500);
+    }
+  }
+  register(userData: RegisterDto): Promise<AuthEntity> {
+    throw new Error("Method not implemented.");
+  }
 }
